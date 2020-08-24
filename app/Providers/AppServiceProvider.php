61487dbd08e8ee42acabe7de2b4afa1133bfd8b1
hana545 <hana.radojcic545@gmail.com>
2020-08-24 19:33:28 +0200

    trying forced https hehe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +23,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-
+        if (env('APP_ENV') !== 'local') {
+            $url->forceScheme('https');
+        }
     }
+
 }
